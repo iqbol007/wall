fdaf243266866defe6cd662bd85945b7f8b41645
iqbol <iqbol@localhost>
2020-03-05 10:42:21 +0300

    awd

--- src/GalleryItemsList/GalleryItemsList.jsx
+++ src/GalleryItemsList/GalleryItemsList.jsx
@@ -8,7 +8,6 @@ export default function GalleryItemsList({ items, loading, error }) {
     if (loading) {
       return (
         <div className="center-loader">
-         
           <div class="spinner-border text-dark" role="status">
             <span class="sr-only">Loading...</span>
           </div>
@@ -18,7 +17,7 @@ export default function GalleryItemsList({ items, loading, error }) {
     if (error) {
       return (
         <div class="alert alert-danger" role="alert">
-          <span className="error-info">Errorchik!!!</span>
+          <span className="error-info">Error!</span>
         </div>
       );
     }
