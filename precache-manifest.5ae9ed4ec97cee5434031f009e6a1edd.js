8c56f5129589ddda7e3d84969d140e55156ff822
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-05 07:21:47 +0000

    Deploy iqbol007/wall to github.com/iqbol007/wall.git:gh-pages

---
+++ precache-manifest.5ae9ed4ec97cee5434031f009e6a1edd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b219946d65aef93d4f3b539d380960fe",
+    "url": "/wall/index.html"
+  },
+  {
+    "revision": "4933ad3a16ecc88e0db7",
+    "url": "/wall/static/css/main.e87803fd.chunk.css"
+  },
+  {
+    "revision": "8b7f76b6e20e54c9eecb",
+    "url": "/wall/static/js/2.90ac0bf6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/wall/static/js/2.90ac0bf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4933ad3a16ecc88e0db7",
+    "url": "/wall/static/js/main.1d95a852.chunk.js"
+  },
+  {
+    "revision": "64cecb723b081ea30f10",
+    "url": "/wall/static/js/runtime-main.708b4dae.js"
+  },
+  {
+    "revision": "f700267aa0a4ef09d8415d45a673735e",
+    "url": "/wall/static/media/word.f700267a.png"
+  }
+]);
